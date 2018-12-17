1fdea0d6256431b3065a75684cc248e379212c4e
Wizzac <cuksebastian@gmail.com>
2018-12-17 19:27:40 -0300

    music player

--- src/index.js
+++ src/index.js
@@ -6,7 +6,44 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+
+
+
+function PlayButton(props) {
+    const className = props.isMusicPlaying ? 'play active' : 'play';
+      return (
+        <a onClick={props.onClick} href="#" title="Play video" className={className}  />
+      );
+    }
+  
+   class Container extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { isMusicPlaying: false };
+    }
+  
+    handleClick(event) {
+      if (this.state.isMusicPlaying) {
+        this.audio.pause();
+      } else {
+        this.audio.play();
+        }
+    };
+  
+  
+      render() {
+        const status = this.state.isMusicPlaying ? 'Playing' : 'Not playing';
+  
+        return (
+          <div>
+            <h1 onClick={this.handleClick.bind(this)}>{ status }</h1>
+            <PlayButton onClick={this.handleClick.bind(this)} isMusicPlaying={this.state.isMusicPlaying} />
+            <audio id="audio" ref={(audioTag) => { this.audio = audioTag }}/>
+          </div>
+        );
+      }
+    }
+   const placeWeWantToPutComponent = document.getElementById('hook');
+   ReactDOM.render(<Container />, placeWeWantToPutComponent);
+
